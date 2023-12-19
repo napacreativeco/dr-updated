a76a3e16092b27de06d7f068a3d79b86f8a29220
throne <contact@throne.work>
2023-12-19 12:10:32 -0800

    working through notes

--- assets/index.js
+++ assets/index.js
@@ -103,7 +103,7 @@
     $('.cart-nav-opener').on('click', function() {
         $('body').addClass('js-my-cart-open');
     });
-    $('.cart-drawer--close').on('click', function() {
+    $('.cart-drawer--close').on('mousedown', function() {
         $('body').removeClass('js-my-cart-open');
     });
 
